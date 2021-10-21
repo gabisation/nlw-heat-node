53b149151ab35ba4007f1d9aeee2e93cacbdcc36
Gabrielly Barros Fonseca <gabrielly.barros@hotmail.com>
2021-10-21 19:47:12 -0300

    creates user profile

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 import { CreateMessageController } from "./controllers/CreateMessageController"
 import { GetMessageController } from "./controllers/GetMessageController"
+import { UserProfileController } from "./controllers/UserProfileController"
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated"
 
 const router = Router()
@@ -9,7 +10,6 @@ const router = Router()
 router.post("/authenticate", new AuthenticateUserController().handle)
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 router.get("/messages/latest", new GetMessageController().handle)
+router.get("/profile", ensureAuthenticated, new UserProfileController().handle)
 
-export {
-  router
-}
+export { router }
